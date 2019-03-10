3e2e6102d7c9770878db167365593f23697902a8
Saikat <saikats_5@yahoo.co.in>
2019-03-10 13:43:02 +0530

    provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './components/routes';
+import configureStore from './store/configureStore';
 //import './styles/styles.css';
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
